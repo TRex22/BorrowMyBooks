127c12e362295ee49db20e03e6d2756aec143691
trex22 <trex22@jasonchalom.com>
2016-08-14 15:39:56 +0200

    fixed install

--- src/install.sh
+++ src/install.sh
@@ -3,7 +3,9 @@
 mkdir logs
 npm install -g gulp
 npm install -g istanbul
+npm install -g bower
 sudo npm install -g gulp
 sudo npm install -g istanbul
+sudo npm install -g bower
 npm install
 bower install
