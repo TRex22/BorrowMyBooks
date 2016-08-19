77847b014c6524870f94c4176a6eb654c8cde711
trex22 <contact@jasonchalom.com>
2016-08-19 23:52:24 +0200

    Please

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo 'prebuild...'
-echo 'setup git'
-git config --global user.email "travis.bot@jasonchalom.com"
-git config --global user.name "travis.bot"
+#echo 'prebuild...'
+#echo 'setup git'
+#git config --global user.email "travis.bot@jasonchalom.com"
+#git config --global user.name "travis.bot"
 
 echo 'move to source folder'
 cd src/
