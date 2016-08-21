20f4f8a63932231563697f9364c1f7147514266e
trex22 <contact@jasonchalom.com>
2016-08-21 22:54:47 +0200

    converted to ejs standard.

--- src/routes/explore.js
+++ src/routes/explore.js
@@ -5,7 +5,25 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('explore', { title: 'Borrow My Books', buildVersion: pkg.version });
+    //TODO: JMC database connection
+    //also system defaults for alt
+    var json = [{
+            "title": "Hearts in Atlantis",
+            "author": "Stephen King",
+            "picURL": "https://upload.wikimedia.org/wikipedia/en/4/49/Dewey_-_The_Small-Town_Library_Cat_Who_Touched_the_World_(book_cover).jpg",
+            "interests": "Fantasy",
+            "edition": "Best edition"
+        },
+
+        {
+            "title": "Odd Thomas",
+            "author": "Dean Koontz",
+            "picURL": "https://upload.wikimedia.org/wikipedia/en/4/49/Dewey_-_The_Small-Town_Library_Cat_Who_Touched_the_World_(book_cover).jpg",
+            "interests": "Fiction",
+            "edition": "Great edition"
+        }
+    ];
+    res.render('explore', { title: 'Borrow My Books', buildVersion: pkg.version, books: json });
 });
 
 module.exports = router;
