6f6e3062f12152012c4c808647d28dd0a04ce8d5
trex22 <contact@jasonchalom.com>
2016-08-21 13:36:11 +0200

    working on fixing this

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -49,19 +49,19 @@ gulp.task('docs', function() {
         .pipe(gulp.dest('docs'));
 });
 
-gulp.task('pre-test', function() {
-    return gulp.src(['lib/**/*.js'])
-        // Covering files
-        .pipe(istanbul())
-        // Force `require` to return covered files
-        .pipe(istanbul.hookRequire());
+gulp.task('pre-test', function () {
+  return gulp.src(['lib/**/*.js'])
+    // Covering files
+    .pipe(istanbul())
+    // Force `require` to return covered files
+    .pipe(istanbul.hookRequire());
 });
 
-gulp.task('test', ['pre-test'], function() {
-    return gulp.src(['test/*.js'])
-        .pipe(mocha())
-        // Creating the reports after tests ran
-        .pipe(istanbul.writeReports())
-        // Enforce a coverage of at least 90%
-        .pipe(istanbul.enforceThresholds({ thresholds: { global: 90 } }));
+gulp.task('test', ['pre-test'], function () {
+  return gulp.src(['test/*.js'])
+    .pipe(mocha())
+    // Creating the reports after tests ran
+    .pipe(istanbul.writeReports())
+    // Enforce a coverage of at least 90%
+    .pipe(istanbul.enforceThresholds({ thresholds: { global: 90 } }));
 });
