3907a9b560529a2bff815c0609d1bbaa99dc9b8a
trex22 <contact@jasonchalom.com>
2016-08-30 02:35:28 +0200

    global config vars for heroku

--- src/config/db.js
+++ src/config/db.js
@@ -18,8 +18,12 @@ try {
         logger.info("test db");
         mongoose.connect(config.mongodbTest, options);
         logger.info("connected to database.");
-    } else {
+    } else if (process.env.NODE_ENV === 'production') {
         logger.info("production db");
+        mongoose.connect(process.env.MONGODB_URI, options); //heroku deploy
+        logger.info("connected to database.");
+    } else {
+        logger.info("development db");
         mongoose.connect(config.mongodb, options);
         logger.info("connected to database.");
     }
