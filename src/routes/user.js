e586a4b44a387d652099ae46beeca1e359ca202f
trex22 <contact@jasonchalom.com>
2016-09-25 20:23:33 +0200

    cleanup after a litter bug - miz

---
+++ src/routes/user.js
@@ -0,0 +1,20 @@
+/* jshint node: true */
+var express = require('express');
+var pkg = require('../package');
+var router = express.Router();
+
+var mongoose = require('../config/db.js').mongoose;
+var dbHelper = require('../services/dbHelper');
+var userHelper = require('../services/userHelper.js');
+var book = require('../models/book');
+
+module.exports = function(app, passport) {
+    app.get('/explore',
+        function(req, res, next) {
+            if (userHelper.auth(req, res, app.locals.site)) {
+                req = userHelper.processUser(req);
+
+                res.render('accounts/profile', { site: app.locals.site, user: req.user });
+            }
+        });
+};
