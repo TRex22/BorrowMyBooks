57c2f3c5a38898b7ae4afe05ef304d18085c8a1d
trex22 <contact@jasonchalom.com>
2016-10-10 00:47:30 +0200

    seedDb.js rather than scratch pad

--- src/seedDb.js
+++ src/seedDb.js
@@ -1,6 +1,14 @@
 /*var clearDb = require('./db/clearDb').go();*/
-var seed = require('./db/seedDb');
+/*var seed = require('./db/seedDb');
 
 setTimeout(function() {
     process.exit();
 }, 5000);
+*/
+
+var clearDb = require('./db/clearDb').go();
+//wait 2 seconds
+
+setTimeout(function() {
+    var seed = require('./db/seedDb');
+}, 2000);
