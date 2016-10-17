bfbfd90eb1ea1e309aa3350d63cee4f708aeca8d
trex22 <contact@jasonchalom.com>
2016-10-17 02:28:54 +0200

    started working on returning books

--- src/services/transactionHelper.js
+++ src/services/transactionHelper.js
@@ -41,6 +41,46 @@ function getTransactions(userId) {
     });
 }
 
+function getTransactionBooks(userId) {
+    return new Promise(function(resolve, reject) {
+        Transaction.find({ $or: [{ fromUserId: userId }, { toUserId: userId }] }, wrap(function*(err, transactions) {
+            /* istanbul ignore next */
+            if (err) {
+                return reject(err);
+            }
+
+            var books = {};
+            var rentedUsrBooks = [];
+            var loanedUsrBooks = [];
+
+            for (var i = 0; i < transactions.length; i++) {
+                try {
+                    if (transactions[i].toUserId === userId + "") {
+                        var loanedUsrBook = yield bookHelper.getBook(transactions[i].bookId);                        
+                        rentedUsrBooks.push(loanedUsrBook);
+                    }
+
+                    if (transactions[i].fromUserId === userId + "") {
+                        var rentedUsrBook = yield bookHelper.getBook(transactions[i].bookId);
+                        if (transactions[i].isRent) {
+                            loanedUsrBooks.push(rentedUsrBook);
+                        }
+                    }
+
+                } catch (e) {
+                    logger.error(e);
+                    reject(e);
+                }
+            }
+
+            books.rentedUsrBooks = rentedUsrBooks;
+            books.loanedUsrBooks = loanedUsrBooks;
+
+            resolve(books);
+        }));
+    });
+}
+
 function getTransaction(transactionId) {
     return new Promise(function(resolve, reject) {
         Transaction.findOne({ _id: transaction }, function(err, transaction) {
@@ -56,5 +96,6 @@ function getTransaction(transactionId) {
 
 module.exports = {
     getTransactions: getTransactions,
+    getTransactionBooks: getTransactionBooks,
     getTransaction: getTransaction
 }
